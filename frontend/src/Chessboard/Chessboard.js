bf6c120e62a9600d2f12d09327e7414620596c8d
Eric Wang <eric.wang@amplitude.com>
2018-08-14 23:43:48 -0700

    remove unnecessary frontend items, add react-chess, set up app layout

---
+++ frontend/src/Chessboard/Chessboard.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Chess from 'react-chess';
+import './Chessboard.css';
+
+class Chessboard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {pieces: Chess.getDefaultLineup()}
+    this.handleMovePiece = this.handleMovePiece.bind(this)
+  }
+
+  handleMovePiece(piece, fromSquare, toSquare) {
+    const newPieces = this.state.pieces
+      .map((curr, index) => {
+        if (piece.index === index) {
+          return `${piece.name}@${toSquare}`
+        } else if (curr.indexOf(toSquare) === 2) {
+          return false // To be removed from the board
+        }
+        return curr
+      })
+      .filter(Boolean)
+
+    this.setState({pieces: newPieces})
+  }
+
+  render() {
+    const {pieces} = this.state
+    return (
+      <div className="Chessboard">
+        <Chess
+          highlightTarget={false}
+          drawLabels={false}
+          lightSquareColor="#b8ddf5"
+          darkSquareColor="#16a0f7"
+          pieces={pieces}
+          onMovePiece={this.handleMovePiece}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chessboard;
---
+++ frontend/src/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className="Sidebar">
+        <div>*current status*</div>
+        <div>*chat messages*</div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
